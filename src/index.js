e7d73cea502972b74fc9554101f240ff70625a03
royderks <10717410+royderks@users.noreply.github.com>
2021-11-24 14:24:04 +0100

    ex-4

--- src/index.js
+++ src/index.js
@@ -1,35 +1,46 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require('apollo-server-core');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const { isTokenValid } = require('./authentication');
 const db = require('./database');
-const AuthDirective = require('./directives');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    const token = req.headers.authorization;
-    const { userId } = isTokenValid(token || '') || {};
-    const user = db.users.get(userId);
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      const token = req.headers.authorization;
+      const { userId } = isTokenValid(token || '') || {};
+      const user = db.users.get(userId);
+  
+      return {
+        token,
+        isAuthenticated: !!user,
+        user,
+      };
+    },
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
 
-    return {
-      token,
-      isAuthenticated: !!user,
-      user,
-    };
-  },
-});
+  // Wait until server is created and started
+  await server.start();
 
-const app = express();
-server.applyMiddleware({ app });
+  const app = express();
+  server.applyMiddleware({ app });
 
-const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(
-    `GraphQL endpoint and playground available at http://localhost:${PORT}${server.graphqlPath}`,
-  );
-});
+  const PORT = 4000;
+
+  app.listen(PORT, () => {
+    console.log(
+      `GraphQL endpoint and playground available at http://localhost:${PORT}${server.graphqlPath}`,
+    );
+  });
+}
+
+startApolloServer();
